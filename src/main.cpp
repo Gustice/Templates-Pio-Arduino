bbe0e7820b4811fc025be38b681681e99d4320d9
Gustice <jakob.ger@gmail.com>
2024-01-27 23:34:31 +0100

    feature: create minimalistic blinking app

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include <Arduino.h>
 
+const int LedPin = LED_BUILTIN;
+const int BlinkTime = 250;
+
 void setup() {
+    pinMode(LedPin, OUTPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
+    Serial.println("ON");
+    digitalWrite(LedPin, HIGH);
+    delay(BlinkTime);
+    Serial.println("OFF");
+    digitalWrite(LedPin, LOW);
+    delay(BlinkTime);
 }
