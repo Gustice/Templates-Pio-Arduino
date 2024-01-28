723c1c706126a88678dcc64f388a64086d5aa456
Gustice <jakob.ger@gmail.com>
2024-01-28 23:39:00 +0100

    feature: Adding debugging support for nano

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include "main.hpp"
 
+#ifdef DEBUGGER_STUB
+#include <app_api.h>
+#include <avr8-stub.h>
+#endif
+
 const int BlinkTime = 50;
 
 #ifndef INVERT_STATUS_LED
@@ -11,18 +16,25 @@ constexpr StatusLed::LedLogic StatusLedLogic = StatusLed::LedLogic::Inverted;
 static StatusLed _statusLed(LED_BUILTIN, StatusLedLogic);
 
 void setup() {
+#ifdef DEBUGGER_STUB
+    debug_init();
+#else
     Serial.begin(9600);
+#endif
     _statusLed.Set(StatusLed::Mode::DoubleFlash);
 }
 
 static RotatingIndex<int> _tickIdx(16);
 
 void loop() {
+#ifndef DEBUGGER_STUB
     if (_tickIdx.IncrementAndCheckIfRevolves()) {
         Serial.println("");
         Serial.print("Tick ");
     }
     Serial.print(".");
+#endif
+
     _statusLed.Tick();
     delay(BlinkTime);
 }
